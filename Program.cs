6a0b7b35ba992b3be1fa25f1a365ad281734d189
“Salil48” <salilchharia@gmail.com>
2023-01-16 18:56:11 +0530

    Welcome to User Registration Program

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace UserRegistration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("User Registration Validating using Regular Expression!");
+            RegexSample regexSample = new RegexSample();
+            regexSample.Validating();
+        }
+    }
+}
